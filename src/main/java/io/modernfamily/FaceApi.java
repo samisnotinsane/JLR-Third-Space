400983099db71185e8cb2d4055705b8fed47b1cf
Sameen Islam <ec14370@qmul.ac.uk>
2016-07-24 06:19:44 +0100

    Implemented JSON parser for consuming API data.

--- src/main/java/io/modernfamily/FaceApi.java
+++ src/main/java/io/modernfamily/FaceApi.java
@@ -1,11 +1,5 @@
 package io.modernfamily;
 
-import com.sun.jersey.core.util.Base64;
-import com.uploadcare.api.Client;
-import com.uploadcare.upload.FileUploader;
-import com.uploadcare.upload.UploadFailureException;
-import com.uploadcare.upload.Uploader;
-import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -15,21 +9,18 @@ import org.apache.http.entity.FileEntity;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
-import org.snmp4j.smi.OctetString;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
-import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URI;
-import java.util.Properties;
 
 /**
  * Created by sameenislam on 24/07/2016.
  *
  * Detect human faces and compare similar ones, organize people into groups
- * according to visual similarity, and identify previously tagged people in images.
+ * according to visual similarity, and detect previously tagged people in images.
  *
  * Uses Microsoft Azure Cognitive Services.
  */
@@ -38,7 +29,7 @@ public class FaceApi {
 
     private static String key = null;
 
-    public static String identify() {
+    public static String detect(String filename) {
         String result = "";
         AzureGetPropertyValues properties = new AzureGetPropertyValues();
 
@@ -66,7 +57,7 @@ public class FaceApi {
             request.setHeader("Ocp-Apim-Subscription-Key", key);
 
             // Open image and process it
-            File file = new File("face.png");
+            File file = new File(filename);
             FileEntity fEntity = new FileEntity(file, "binary/octet-stream");
             String bs = EntityUtils.toString(fEntity);
 
@@ -89,13 +80,72 @@ public class FaceApi {
             System.out.println(e.getMessage());
         }
 
-        return result;
+        String faceID = "";
+
+        // Parse JSON and extract faceId
+        JSONArray jsonArray = new JSONArray(result);
+        for(int i=0; i<1; i++)
+            faceID = jsonArray.getJSONObject(i).getString("faceId");
+
+//        JSONObject obj = new JSONObject(jsonArray);
+//        String n = obj.getJSONObject("face").getString("faceId");
+        System.out.println("faceId = " + faceID);
+
+        return faceID;
     }
 
+    public static void verify(String strA, String strB) {
+
+        HttpClient httpclient = HttpClients.createDefault();
+
+        try
+        {
+            URIBuilder builder = new URIBuilder("https://api.projectoxford.ai/face/v1.0/verify");
+
+
+            URI uri = builder.build();
+            HttpPost request = new HttpPost(uri);
+            request.setHeader("Content-Type", "application/json");
+            request.setHeader("Ocp-Apim-Subscription-Key", key);
+
+
+            // Request body
+            StringEntity reqEntity = new StringEntity("{\n" +
+                    "    \"faceId1\":\"" + strA + "\",\n" +
+                    "    \"faceId2\":\"" + strB + "\" \n" +
+                    "}");
+            request.setEntity(reqEntity);
+
+            HttpResponse response = httpclient.execute(request);
+            HttpEntity entity = response.getEntity();
+
+            if (entity != null)
+            {
+                System.out.println(EntityUtils.toString(entity));
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+
+    }
 
     public static void main(String[] args)
     {
-        System.out.println(identify());
+
+        // Initialise identity (of stored face) to get first faceId
+        System.out.println(detect("face.png"));
+
+        // Take new image, run recognition on it and get new faceId
+        try {
+            CaptureFacial.capture("face1.png");
+        } catch (IOException e) {
+            System.out.println("[System exception] Failed to capture image for face detection");
+        }
+        System.out.println(detect("face1.png"));
+//        verify();
+        // Compare two faceId using Verify api
 
     }
 
---
+++ src/main/java/io/modernfamily/JsonParser.java
@@ -0,0 +1,10 @@
+package io.modernfamily;
+
+/**
+ * Created by sameenislam on 24/07/2016.
+ * Parses JSON.
+ */
+public class JsonParser {
+
+
+}
